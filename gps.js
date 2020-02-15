800857231e586cd9aadaaa9392ba85d9c4d2d436
Injamul Haque Rashik <ihrashik@gmail.com>
2020-02-16 02:12:37 +0600

    gps.js file Added!

---
+++ gps.js
@@ -0,0 +1,16 @@
+x = navigator.geolocation;
+x.getCurrentPosition(success, failure);
+function success(position) {
+  var myLat = position.coords.latitude;
+  var myLong = position.coords.longitude;
+
+  var coords = new google.map.LatLng(myLat, myLong);
+  var mapOptions = {
+    zoom: 9,
+    center: coords,
+    mapTypeId: google.maps.mapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  var marker = new google.maps.Marker({ map: map, position: coords });
+}
+function failure() {}
