804a2bd186de6faa84ab2199611bbefc1803f33b
Injamul Haque Rashik <ihrashik@gmail.com>
2020-02-20 10:42:06 +0600

    GPS Tracker Updated

--- gps.js
+++ gps.js
@@ -1,16 +1,29 @@
 x = navigator.geolocation;
+
 x.getCurrentPosition(success, failure);
-function success(position) {
+
+function success(position)
+
+{
   var myLat = position.coords.latitude;
   var myLong = position.coords.longitude;
 
-  var coords = new google.map.LatLng(myLat, myLong);
+  //alert(myLat);
+
+  var coords = new google.maps.LatLng(myLat, myLong);
+
   var mapOptions = {
-    zoom: 9,
+
+    zoom: 11,
     center: coords,
-    mapTypeId: google.maps.mapTypeId.ROADMAP
-  };
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+
   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-  var marker = new google.maps.Marker({ map: map, position: coords });
+  var marker = new google.maps.Marker({
+    map: map,
+    position: coords
+  });
 }
+
 function failure() {}
